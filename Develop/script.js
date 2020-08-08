9d73bd1ef5e3784c590a8059d202096a11717254
Jeffrey Choi <jeffrey1choi@gmail.com>
2020-08-07 17:09:23 -0700

    Added conditionals, need to adjust how to capture correct value

--- Develop/script.js
+++ Develop/script.js
@@ -1,48 +1,76 @@
+var addToTasks = []
+
+
 function currentDate () {
     var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 
     $("#currentDay").text(currentTime);
   }
-
+  
 setInterval(currentDate,1000);
 
 currentDate();
 
 
-for (var i = 0; i < 12; i++) {
+for (var i = 0; i < 25; i++) {
+
+    var hourText = i + 1;
+
+    var hourString = "hour";
+
+    var hourID = hourText + hourString;
 
     const rowElement = $("<div class = 'row'>");
 
     rowElement.appendTo(".timeContainer");
-
-    const hourDiv = $(`<div class = 'col-md-1 hour' id = ${hourText + hourString}>`);
-
-    var hourText = i + 9
-
-    var hourString = "hour"
-
-    var hourID = hourText + hourString
-
-    if ( i < 3 ) {
+    
+    const hourDiv = $(`<div class = 'col-md-1 hour' id = ${hourText}>`);
+    
+    if ( i < 12 ) {
 
         hourDiv.text(hourText + "AM")
 
-    } else if ( i === 3) {
-        hourDiv.text(hourText + "PM")
-    } else 
-        hourDiv.text((hourText-12) + "PM")
+    } else if ( i === 12) {
+        hourDiv.text((hourText-1) + "PM")
+    } else if( i === 24) {
+        hourDiv.text((hourText - 13) + "AM")
+    } else {
+        hourDiv.text((hourText - 13) + "PM")
+    }
+        
 
 
     hourDiv.appendTo(rowElement);
+    
 
     // const saveImg = $("<img src = ' ../Assets/saveBtn.ico' alt = 'floppy disk icon'>")
 
     // saveImg.appendTo(hourDiv)
 
-    const timeBlockDiv = $(`<input type = 'text' class ='col-md-10 time-block' id = ${hourID}>`)
+    const timeBlockDiv = $(`<input type = 'text' class ='col-md-10 time-block' id = ${hourID} value = ${hourID}>`)
 
     timeBlockDiv.appendTo(rowElement);
 
+    const currentHour = moment().add(1, "hours").format("H")
+
+    const adjustedHour = currentHour + "hour"
+
+    var hourElement = $(`#${hourID}`)
+
+    var hourVal = hourElement.val()
+
+    console.log(hourElement)
+
+    if ( hourVal < adjustedHour) {
+        hourElement.addClass("past")
+    } else if (hourVal === adjustedHour) {
+        hourElement.addClass("present")
+    } else if (hourVal > adjustedHour) {
+        hourElement.addClass("future")
+    }
+
+
+    
     const saveBtn = $(`<button class='saveBtn col-md-1' value = ${hourID}>`);
 
     saveBtn.on("click", function(event) {
@@ -54,58 +82,52 @@ for (var i = 0; i < 12; i++) {
         var input = $(`#${Id}`).val() 
 
         localStorage.setItem(Id, input);
+
         
     })
 
     saveBtn.appendTo(rowElement)
+    
+    renderTasks();
+    
+    // aChristmasCarol();
 
-    var currentTime = moment().format("h")
-
-    var timeBlockElement = $(`#${hourID}`)
-
-    console.log(timeBlockElement);
-
-    // // const saveImg = $("<img src = ' ../Assets/saveBtn.ico' alt = 'floppy disk icon'>")
-
-    // // saveImg.appendTo(hourDiv)
-
+    // console.log(aChristmasCarol)
 }
 
-function renderTask () {
 
+function renderTasks () {
+    const keys = Object.entries(localStorage)
 
-    for (var i = 0; i < localStorage.length; i++) {
+    keys.forEach(key => {
+        $("#" + key[0]).val(key[1])
+    });
+}
 
-        var taskItem = i + 9
+function aChristmasCarol() {
 
-        var inputTask = localStorage.getItem(`${taskItem}`) 
+    for ( var i = 0; i < 25; i++) {
 
-        $(`#${taskItem}`).val(inputTask)
+        var hourText = i + 1;
+
+        var hourString = "hour";
+    
+        var hourID = hourText + hourString;
+
+        const currentHour = moment().add(1, "hours").format("H")
+
+        const adjustedHour = currentHour + "hour"
+    
+        var hourElement = $(`#${hourID}`)
+    
+        if (adjustedHour === hourElement) {
+            hourElement.addClass("present")
+
+            console.log(adjustedHour)
 
         }
 
     }
+    
 
-$(document).ready(function () {
-
-    $(".saveBtn").on("click", function(event) {
-
-        for (var i = 0; localStorage.length; i++) {
-
-        event.preventDefault();
-
-        var textID = i + 9
-
-        var input = $(`#${textID}`).val() 
-
-        console.log(textID)
-
-        localStorage.setItem(`${textID}`, input);
-
-        }
-
-    })
-
-    renderTask();
-
-});
+}
